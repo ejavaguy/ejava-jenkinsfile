ff4aad3e4956e5facdc1c8d420cfc6d20bc4451b
jim <ejavaguy@gmail.com>
2019-07-10 22:47:52 -0400

    added missing configFileProvider

--- vars/mavenContainer.groovy
+++ vars/mavenContainer.groovy
@@ -2,6 +2,8 @@
 
 def call(Closure body) {
     container('maven') {
+      configFileProvider([configFile(fileId: env.SETTINGS_XML,
+			  variable: 'MAVEN_SETTINGS_XML')]) {
 	withCredentials([sshUserPrivateKey(credentialsId: 'webdev-publish-key',
 							   keyFileVariable: 'SSH_KEYS',
 							   passphraseVariable: 'SSH_PASS',
@@ -10,5 +12,6 @@ def call(Closure body) {
                 body()
 	    }
 	}
+      }
     }
 }
