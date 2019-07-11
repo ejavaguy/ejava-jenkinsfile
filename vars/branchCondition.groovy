0a3b562d4ef79a1568042797bcce1d26dcf0f9d0
jim <ejavaguy@gmail.com>
2019-07-11 06:11:48 -0400

    removed if

--- vars/branchCondition.groovy
+++ vars/branchCondition.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(Closure doTrue, Closure doFalse) {
-  if (branch 'jenkins-master') {
+  branch 'jenkins-master' {
     doTrue()
-  } else {
-    doFalse()
-  }
+  } 
 }
