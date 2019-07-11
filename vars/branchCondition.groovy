2bfe9a09b196d7d8615559190de9f40394a93f49
jim <ejavaguy@gmail.com>
2019-07-11 06:29:57 -0400

    using raw conditional

--- vars/branchCondition.groovy
+++ vars/branchCondition.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(Closure doTrue, Closure doFalse) {
-  branch 'jenkins-master' {
-    doTrue()
-  } 
+def call(script, branch, Closure doTrue, Closure doFalse) {
+  if (script.env.BRANCH_NAME == "${branch}") {
+    doTrue(script)
+  } else {
+    doFalse(script)
+  }
 }
