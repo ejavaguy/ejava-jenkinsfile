0c59f74e42b72f600a29d9a33c56d33252e9c4a9
jim <ejavaguy@gmail.com>
2021-08-22 11:29:29 -0400

    updating maven ssh tools

---
+++ vars/sshMaven.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+def call(String credentialsId, Closure body) {
+	  configFileProvider([configFile(fileId: env.SETTINGS_ID, variable: 'MAVEN_SETTINGS_PATH')]) {
+			withCredentials([sshUserPrivateKey(credentialsId: credentialsId,
+									   keyFileVariable: 'SSH_KEYS',
+									   passphraseVariable: 'SSH_PASS',
+									   usernameVariable: 'SSH_USER')]) {
+				sshagent( credentials:[credentialsId])   {
+						body()
+				}
+			}
+	  }
+}
