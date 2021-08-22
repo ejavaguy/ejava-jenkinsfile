5dcfdcb59d707639c8791dbd7bed93ff293e399f
jim <ejavaguy@gmail.com>
2021-08-22 12:49:50 -0400

    corrected variable reference

--- vars/sshMaven.groovy
+++ vars/sshMaven.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String credentialsId, Closure body) {
-	  configFileProvider([configFile(fileId: env.SETTINGS_ID, variable: 'MAVEN_SETTINGS_PATH')]) {
+	  configFileProvider([configFile(fileId: env.MAVEN_SETTINGS_ID, variable: 'MAVEN_SETTINGS_PATH')]) {
 			withCredentials([sshUserPrivateKey(credentialsId: credentialsId,
 									   keyFileVariable: 'SSH_KEYS',
 									   passphraseVariable: 'SSH_PASS',
