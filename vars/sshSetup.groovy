585e4dbde92f1475a81d806af92a6b065ce7c0d4
jim <ejavaguy@gmail.com>
2021-08-22 11:47:36 -0400

    removed straight groovy calls

--- vars/sshSetup.groovy
+++ vars/sshSetup.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String host) {
-	if (null!=host && !host.isEmpty()) {
-		sh 'mkdir -p ~/.ssh'
-		sh "ssh-keyscan -t rsa ${host} >> ~/.ssh/known_hosts"
-		sh 'chmod 0700 ~/.ssh && chmod 640 ~/.ssh/known_hosts'
-	}
+	sh 'mkdir -p ~/.ssh'
+	sh "ssh-keyscan -t rsa ${host} >> ~/.ssh/known_hosts"
+	sh 'chmod 0700 ~/.ssh && chmod 640 ~/.ssh/known_hosts'
 }
