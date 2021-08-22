aade8b303c61bb0ee8742f8968f8f79de09ed133
jim <ejavaguy@gmail.com>
2021-08-22 11:52:35 -0400

    making noop capable

--- vars/sshSetup.groovy
+++ vars/sshSetup.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(String host) {
-	sh 'mkdir -p ~/.ssh'
-	sh "ssh-keyscan -t rsa ${host} >> ~/.ssh/known_hosts"
-	sh 'chmod 0700 ~/.ssh && chmod 640 ~/.ssh/known_hosts'
+	if (null!=host && !host.isEmpty()) {
+		sh 'mkdir -p ~/.ssh'
+		sh "ssh-keyscan -t rsa ${host} >> ~/.ssh/known_hosts"
+		sh 'chmod 0700 ~/.ssh && chmod 640 ~/.ssh/known_hosts'
+	}
 }
